c43842cb38aea313a276259ba379582c73f93b85
Kanstantsin Kulak <Kanstantsin_Kulak@epam.com>
2018-06-15 16:16:14 +0300

    finish tasks

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -2,44 +2,28 @@ package runner;
 
 import org.testng.ITestNGListener;
 import org.testng.TestNG;
-import org.testng.xml.XmlClass;
 import org.testng.xml.XmlSuite;
-import org.testng.xml.XmlTest;
-import tests.basicTests.IndependentTest;
 import utils.constants.GroupProps;
-import utils.listeners.TestListener;
+import utils.listeners.InvokedMethodListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TestRunner {
     public static void main(String[] args) {
+        ITestNGListener listener = new InvokedMethodListener();
         TestNG tng = new TestNG();
-        ITestNGListener iTestNGListener = new TestListener();
-        tng.addListener(iTestNGListener);
-
-        XmlClass xmlClass = new XmlClass();
-        xmlClass.setClass(IndependentTest.class);
-        List<XmlClass> xmlClasses = new ArrayList<>();
-        xmlClasses.add(xmlClass);
-
-        XmlTest test = new XmlTest();
-        test.setName("Group " + GroupProps.FIRST_TEST_GROUP + " test");
-        List<String> groups = new ArrayList<>();
-        groups.add(GroupProps.FIRST_TEST_GROUP);
-        test.setIncludedGroups(groups);
-        test.setClasses(xmlClasses);
-
-        List<XmlTest> tests = new ArrayList<>();
-        tests.add(test);
-
+        tng.addListener(listener);
         XmlSuite suite = new XmlSuite();
-        suite.setName("Run group " + GroupProps.FIRST_TEST_GROUP);
-        suite.setTests(tests);
+        suite.setName("page group test");
+        suite.addIncludedGroup(GroupProps.FIRST_TEST_GROUP);
+        List<String> files = new ArrayList<>(new ArrayList<String>() {{
+            add("runGroupTest.xml");
+        }});
+        suite.setSuiteFiles(files);
         List<XmlSuite> suites = new ArrayList<>();
         suites.add(suite);
         tng.setXmlSuites(suites);
-
         tng.run();
     }
 }
